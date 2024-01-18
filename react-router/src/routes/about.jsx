d66327d7b446185777dee3a7b72542ea2d33652d
viciorog <148492158+viciorog@users.noreply.github.com>
2024-01-18 18:01:16 +0100

    Add files via upload

---
+++ react-router/src/routes/about.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+export default function About() {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>
+        Welcome to our website! We are a team of passionate individuals
+        dedicated to providing valuable information and services.
+      </p>
+      <p>Our mission is to [insert your mission or purpose here].</p>
+    </div>
+  );
+}
