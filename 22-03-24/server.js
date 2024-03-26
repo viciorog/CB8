60e778ed8a11713dc722d5e3cc359a412022739a
viciorog <148492158+viciorog@users.noreply.github.com>
2024-03-26 19:03:02 +0100

    Add files via upload

---
+++ 22-03-24/server.js
@@ -0,0 +1,150 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("./models/users");
+
+mongoose.connect(process.env.DATABASE_URL);
+
+const app = express();
+
+app.use(express.json());
+const db = mongoose.connection;
+
+db.on("error", (err) => console.error(err));
+db.once("open", () => console.log("Connessione stabilita!"));
+
+const authenticationToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) {
+    return res.status(401).json({ message: "Inserire un token valido." });
+  } else {
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
+      if (err)
+        return res
+          .status(403)
+          .json({ message: "Non hai i permessi di verifica." });
+      req.user = user;
+      next();
+    });
+  }
+};
+
+app.get("/", (req, res, next) => {
+  res.send("<h1>HOMEPAGE</h1>");
+});
+
+app.post("/login", async (req, res, next) => {
+  const user = await User.findOne({
+    username: req.body.username,
+  });
+
+  if (user === null) {
+    return res.status(400).json({ message: "L'utente non esiste" });
+  }
+
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      const accessToken = jwt.sign(
+        { username: user.username },
+        process.env.ACCESS_TOKEN
+      );
+
+      res.json({ accessToken });
+    }
+  } catch (error) {
+    res.json({ message: "Autorizzazione fallita" });
+  }
+});
+
+app.post("/register", async (req, res, next) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    const newUser = await user.save();
+    res.status(201).json({ message: "Utente registrato", user: newUser });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "La registrazione non è andata a buon fine." });
+    console.log(error);
+  }
+});
+
+app.get("/users", authenticationToken, async (req, res, next) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+app.get("/users/:id", authenticationToken, async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+app.post("/users", async (req, res, next) => {
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    firstName: req.body.firstname,
+    lastName: req.body.lastname,
+    age: req.body.age,
+  });
+
+  try {
+    const newUser = await user.save();
+    res.status(201).json({ newUser });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+app.delete("/users/:id", async (req, res, next) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    res.status(201).json({ message: "L'utente è stato cancellato" });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+app.put("/users/:id", async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+    } else {
+      const { username, email, password, age } = req.body;
+      if (username && email && password && age) {
+        user.username = username;
+        user.email = email;
+        user.password = password;
+        user.age = age;
+
+        await user.save();
+        res.status(201).json({ message: "L'utente è stato modificato!" });
+      }
+    }
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+app.listen(3001);
